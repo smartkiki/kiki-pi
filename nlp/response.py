2614159544cf7c92e248177741dbdb33d9f654b1
aranga1 <aranga@purdue.du>
2017-10-31 18:58:51 -0400

    added code for intent and entity retreival

--- nlp/response.py
+++ nlp/response.py
@@ -1,6 +1,8 @@
 import sys, json, requests
 from flask import Flask, request
 
+CLIENT_ACCESS_TOKEN = '66536bec831d476490738b7c7860ff73'
+
 try:
     import apiai
 except ImportError:
@@ -8,6 +10,9 @@ except ImportError:
         os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
     )
     import apiai
+
+ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
+
 class Response(object):
 	"""
 	An object responsible for all recast related functionality
@@ -16,10 +21,8 @@ class Response(object):
 	def get_response(self, user_req):
 		# Client Access Token for accessing our API AI Bot
 		# I'll put the token in a common vars file in the next commit.
-		CLIENT_ACCESS_TOKEN = '66536bec831d476490738b7c7860ff73'
 
 		# An endpoint to ApiAi, an object used for making requests to a particular agent.
-		ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
 
 		# Ranga/eeps text goes here.
 		request = ai.text_request()
@@ -40,4 +43,17 @@ class Response(object):
 	def get_intent(self, user_req):
 		#Edla please get intent and entities and send them both as follows:
 		# return intent, entities <- here entities is an array of entities
-		pass
+		request = ai.text_request()
+		request.query = user_req
+
+		# Receiving the response.
+		response = json.loads(request.getresponse().read().decode('utf-8'))
+		responseStatus = response['status']['code']
+		intent = response['metadata']['intentName']
+		entities = []
+
+		for i in response['result']['parameters']:
+			entities.append(i)
+
+		return intent, entities
+		
