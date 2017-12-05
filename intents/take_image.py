2ef74abf1e64c19738a815771e64522a11d6c07e
aranga1 <aranga@purdue.du>
2017-12-05 18:11:31 -0500

    commit to master

--- intents/take_image.py
+++ intents/take_image.py
@@ -1,9 +1,20 @@
 from ..camera.image_capture import capture
+import requests
 
 CHATBOT_URL = 'http://localhost:8082/uploadimage'
 
 def execute_intent(entities):
-    image_file = capture()
-    files = {'file': open(image_name)}
-    response = requests.post(CHATBOT_URL, files=files)
+    #image_name = capture()
+    #files = {'file': open(image_name)}
+    print "Got to image capture intent"
+    data = _create_json_for_request()
+    response = requests.post(CHATBOT_URL, json=data)
+    print "got to after response"
     return 'Done! The image has successfully been sent to your mobile device'
+
+def _create_json_for_request():
+	data = { 
+		"fbid" : "1725030900888096",
+		"url" : "https://www.w3schools.com/css/trolltunga.jpg"
+	}
+	return data
